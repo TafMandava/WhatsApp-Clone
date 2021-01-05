7460e44015ebe8550b35d4f14123acf2b123a1c6
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-05 21:47:16 +0200

    Initial structure

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+/*
+    Use when connecting to firebase for authentication and hosting
+*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3N0TXVsfw2OKNzJVawd-G_cnQiYrrLwg",
+    authDomain: "whatsapp-clone-89063.firebaseapp.com",
+    projectId: "whatsapp-clone-89063",
+    storageBucket: "whatsapp-clone-89063.appspot.com",
+    messagingSenderId: "622152965561",
+    appId: "1:622152965561:web:7cebfd2374785d38566a68"
+  };
