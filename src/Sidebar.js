164d9b7606d91884439c6ff9691c43c0c1ed5294
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-05 23:58:23 +0200

    Wrapping Donut icon inside the IconButton to make it clickable

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Sidebar.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+           <div className="sidebar__header">
+               <div className="sidebar__headerLeft">
+                   <Avatar
+                       src=""          
+                   />
+               </div>
+               <div className="sidebar__headerRight">
+                   {/* Wrapping Donut icon inside the IconButton to make it clickable  */}
+                   <IconButton>
+                       <DonutLargeIcon />
+                   </IconButton>
+                   <ChatIcon />
+               </div>               
+           </div>
         </div>
     );
 }
