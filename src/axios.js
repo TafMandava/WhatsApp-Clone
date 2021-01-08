77a82eda8ac53451ca1a238d85911a925e38a17e
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-08 18:15:38 +0200

    Changing the base URL

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,40 @@ const instance = axios.create({
         This is the base url for any request that we make
         However, we have to change the value once it's been deployed to Herouku
     */
-    baseURL: "http://localhost:9000"
+    baseURL: "https://whatsapp--mern--backend.herokuapp.com"
 });
 
 export default instance;
+
+/*
+    Deploying Backend Application to Heroku - https://dashboard.heroku.com/apps
+
+    At most 5 builds - Does not require credit card details
+    New
+        -- Create new app
+            -- App name
+            -- Choose a region
+            -- Create app
+
+    Deployment method
+        -- Opening up terminal in backend project
+
+    npm install -g heroku
+    heroku --version
+
+    heroku login
+
+    heroku git:remote -a whatsapp--mern--backend
+
+    git add --all or git add .
+    git status
+    git commit -m "WhatsApp MERN"
+    git push heroku master
+
+    heroku logs --tail
+
+    Heroku Settings
+        -- Scroll down to Domains
+            --  Type in browser https://whatsapp--mern--backend.herokuapp.com/
+                    -- It should say "Hello World!"
+*/
