f6f59ab99cef6ec27de6f9b2db972587fb55ce52
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-08 08:36:10 +0200

    Setting up front end post

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@material-ui/core';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
@@ -6,8 +6,34 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
+import axios from './axios';
 
 function Chat({ messages }) {
+
+    const [input, setInput] = useState('');
+
+    const sendMessage = async (e) => {
+        /*
+            Prevent the refresh on submitting the button
+        */
+        e.preventDefault();
+
+        /* 
+            Making the actual api call
+            Go ahead and post a new message
+        */
+        await axios.post('/api/v1/messages/new', {
+            message: input,
+            name: "Tafadzwa Mandava",
+            timestamp: "Fri, 08 Jan 2021 08:15:18 GMT",
+            received: false
+        });
+
+        setInput('');
+    };
+
+    console.log("Input >>> ", input);
+    
     return (
         <div className="chat">
             <div className="chat__header">
@@ -51,11 +77,16 @@ function Chat({ messages }) {
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
+                    {/*
+                        We need to keep track of what we tyoe in the input field
+                    */}
                     <input
+                        value={input}
+                        onChange = {e => setInput(e.target.value)}
                         placeholder="Type a message"
                         type="text"
                     />
-                    <button type="submit">Send</button>
+                    <button onClick={sendMessage} type="submit">Send</button>
                 </form>
                 <MicIcon />
             </div>
